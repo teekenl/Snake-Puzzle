f6ba044bb1509648807186f6884b60356cbb6b75
TeeKenLau <dknight1938@hotmail.com>
2017-08-24 09:56:16 +1000

    Comment added

--- snake.js
+++ snake.js
@@ -18,6 +18,7 @@ function game(){
     snake_x+=x_move;
     snake_y+=y_move;
 
+    // Snake shows up on other side if it moves beyond the wall.
     if(snake_x<0) {
         snake_x = tc-1;
     }
@@ -27,11 +28,17 @@ function game(){
     if(snake_y>tc-1){
         snake_y = 0;
     }
+    if(snake_y<0) {
+        snake_y = tc-1;
+    }
+
+    // Draw a gaming area.
     ctx.fillStyle="black";
     ctx.fillRect(0,0,canv.width,canv.height);
 
+
+    // Draw a snake.
     ctx.fillStyle="lime";
-    console.log(trail);
     for(var i = 0; i<trail.length;i++) {
         ctx.fillRect(trail[i].x*grid_size,trail[i].y*grid_size,grid_size-2,grid_size-2);
         if(trail[i].x===snake_x && trail[i].y===snake_y){
@@ -39,20 +46,22 @@ function game(){
         }
     }
 
+    // Eliminate the extra part of snake.
     trail.push({x:snake_x,y:snake_y});
     while(trail.length>tail) {
         trail.shift();
     }
 
+    // Lengthen the snake if it hits apple.
     if(apple_x===snake_x && apple_y===snake_y) {
         tail++;
         apple_x = Math.floor(Math.random()*tc);
         apple_y = Math.floor(Math.random()*tc);
     }
+
     ctx.fillStyle="red";
     ctx.fillRect(apple_x*grid_size,apple_y*grid_size,grid_size-2,grid_size-2);
 }
-
 
 // Keydown event handler
 function keyPush(e) {
